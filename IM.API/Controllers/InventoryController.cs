86877d63e6e3d2277a4f11c03f470513c31c35a5
getsbuffer <accollins1337@gmail.com>
2024-07-30 20:52:25 -0400

    Write logic for Controllers and EC

---
+++ IM.API/Controllers/InventoryController.cs
@@ -0,0 +1,65 @@
+﻿using IM.API.EC;
+using IM.Library.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly InventoryEC _inventoryEC;
+
+        public InventoryController()
+        {
+            _inventoryEC = new InventoryEC();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ShopItemDTO>> GetAllItems()
+        {
+            var items = _inventoryEC.GetAllItems();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ShopItemDTO> GetItemById(int id)
+        {
+            var item = _inventoryEC.GetItemById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<ShopItemDTO> AddItem([FromBody] ShopItemDTO newItem)
+        {
+            var addedItem = _inventoryEC.AddItem(newItem);
+            return CreatedAtAction(nameof(GetItemById), new { id = addedItem.Id }, addedItem);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(int id, [FromBody] ShopItemDTO updatedItem)
+        {
+            var result = _inventoryEC.UpdateItem(id, updatedItem);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(int id)
+        {
+            var result = _inventoryEC.DeleteItem(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ IM.API/EC/InventoryEC.cs
@@ -0,0 +1,54 @@
+﻿using IM.Library.DTO;
+
+namespace IM.API.EC
+{
+    public class InventoryEC
+    {
+        private static List<ShopItemDTO> _inventory = new List<ShopItemDTO>();
+
+        public IEnumerable<ShopItemDTO> GetAllItems()
+        {
+            return _inventory;
+        }
+
+        public ShopItemDTO GetItemById(int id)
+        {
+            return _inventory.FirstOrDefault(item => item.Id == id);
+        }
+
+        public ShopItemDTO AddItem(ShopItemDTO newItem)
+        {
+            newItem.Id = _inventory.Any() ? _inventory.Max(item => item.Id) + 1 : 1;
+            _inventory.Add(newItem);
+            return newItem;
+        }
+
+        public bool UpdateItem(int id, ShopItemDTO updatedItem)
+        {
+            var existingItem = _inventory.FirstOrDefault(item => item.Id == id);
+            if (existingItem == null)
+            {
+                return false;
+            }
+
+            existingItem.Name = updatedItem.Name;
+            existingItem.Desc = updatedItem.Desc;
+            existingItem.Price = updatedItem.Price;
+            existingItem.Amount = updatedItem.Amount;
+
+            return true;
+        }
+
+        public bool DeleteItem(int id)
+        {
+            var item = _inventory.FirstOrDefault(i => i.Id == id);
+            if (item == null)
+            {
+                return false;
+            }
+
+            _inventory.Remove(item);
+            return true;
+        }
+    }
+}
