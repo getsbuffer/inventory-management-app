d60e0a1a4c2bd128148b0fde8637ec240f325e4f
getsbuffer <accollins1337@gmail.com>
2024-07-30 21:50:29 -0400

    Move logic for importing, marking down, BOGO, and tax rate into mainpage.

--- IM.MAUI/ViewModels/InventoryManagementViewModel.cs
+++ IM.MAUI/ViewModels/InventoryManagementViewModel.cs
@@ -31,11 +31,7 @@ namespace IM.MAUI.ViewModels
         public ICommand UpdateItemCommand { get; }
         public ICommand DeleteItemCommand { get; }
         public ICommand NavigateToMainMenuCommand { get; }
-        public ICommand ShowMenuCommand { get; }
-        public ICommand ImportCsvCommand { get; }
-        public ICommand ConfigureTaxRateCommand { get; }
-        public ICommand ConfigureBogoCommand { get; }
-        public ICommand MarkDownItemCommand { get; }
+    
 
         public InventoryManagementViewModel(ShopItemService shopItemService, ShoppingCartProxy shoppingCartProxy)
         {
@@ -48,116 +44,7 @@ namespace IM.MAUI.ViewModels
             UpdateItemCommand = new Command(async () => await UpdateItem());
             DeleteItemCommand = new Command(async () => await DeleteItem());
             NavigateToMainMenuCommand = new Command(async () => await Shell.Current.GoToAsync(nameof(MainPage)));
-            ShowMenuCommand = new Command(async () => await ShowMenu());
-            ImportCsvCommand = new Command(async () => await ImportCsv());
-            ConfigureTaxRateCommand = new Command(async () => await ConfigureTaxRate());
-            ConfigureBogoCommand = new Command(async () => await ConfigureBogo());
-            MarkDownItemCommand = new Command(async () => await MarkDownItem());
-        }
 
-        private async Task ShowMenu()
-        {
-            var action = await Application.Current.MainPage.DisplayActionSheet("Options", "Cancel", null, "Import CSV", "Configure Tax Rate", "Configure BOGO", "Mark down an item");
-
-            if (action == "Import CSV")
-            {
-                await ImportCsv();
-            }
-            else if (action == "Configure Tax Rate")
-            {
-                await ConfigureTaxRate();
-            }
-            else if (action == "Configure BOGO")
-            {
-                await ConfigureBogo();
-            }
-            else if (action == "Mark down an item")
-            {
-                await MarkDownItem();
-            }
-        }
-
-        private async Task ImportCsv()
-        {
-            string filePath = await PickCsvFileAsync();
-            if (filePath != null)
-            {
-                _shopItemService.ImportItemsFromCsv(filePath);
-                ReadItems();
-            }
-        }
-
-        private async Task ConfigureTaxRate()
-        {
-            string result = await Application.Current.MainPage.DisplayPromptAsync("Configure Tax Rate", "Enter the sales tax rate (e.g., 0.05 for 5%):", initialValue: _shoppingCartProxy.TaxRate.ToString());
-
-            if (decimal.TryParse(result, out decimal taxRate))
-            {
-                _shoppingCartProxy.TaxRate = taxRate;
-            }
-        }
-
-        private async Task ConfigureBogo()
-        {
-            string result = await Application.Current.MainPage.DisplayPromptAsync("Configure BOGO", "Enter the ID of the item to make BOGO:");
-
-            if (int.TryParse(result, out int itemId))
-            {
-                var item = _shopItemService.GetItemById(itemId);
-                if (item != null)
-                {
-                    item.IsBogo = true;
-                    _shopItemService.UpdateItem(item);
-                    ReadItems();
-                }
-            }
-        }
-
-        private async Task MarkDownItem()
-        {
-            string idResult = await Application.Current.MainPage.DisplayPromptAsync("Mark Down Item", "Enter the ID of the item to mark down:");
-            if (!int.TryParse(idResult, out int itemId))
-            {
-                return;
-            }
-
-            string percentResult = await Application.Current.MainPage.DisplayPromptAsync("Mark Down Item", "Enter the percentage to mark down (e.g., 0.10 for 10%):");
-            if (!decimal.TryParse(percentResult, out decimal markdownPercentage))
-            {
-                return;
-            }
-
-            var item = _shopItemService.GetItemById(itemId);
-            if (item != null)
-            {
-                item.Price -= item.Price * markdownPercentage;
-                _shopItemService.UpdateItem(item);
-                ReadItems();
-            }
-        }
-
-        public async Task<string> PickCsvFileAsync()
-        {
-            try
-            {
-                var result = await FilePicker.PickAsync(new PickOptions
-                {
-                    PickerTitle = "Pick a CSV file",
-                    FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
-                    {
-                        { DevicePlatform.WinUI, new[] { ".csv" } }
-                    })
-                });
-
-                if (result == null)
-                    return null;
-
-                return result.FullPath;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
         }
 
         private async Task CreateItem()
